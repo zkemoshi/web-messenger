ae8a18061b37aa271ee17fd313d86830edd28bb9
Zakaria Moshi <zkemoshi@gmail.com>
2021-11-03 10:18:32 +0300

    build

---
+++ build/precache-manifest.56e0ecbb005e10abc240a738056e2893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dafb5ad261e0d4c4ccb5b48deb66b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f48f6a81804a5460192",
+    "url": "/static/css/main.b05b359c.chunk.css"
+  },
+  {
+    "revision": "09b754c53df6c86cffe0",
+    "url": "/static/js/2.cfa11691.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.cfa11691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f48f6a81804a5460192",
+    "url": "/static/js/main.b854dfea.chunk.js"
+  },
+  {
+    "revision": "5452c39493cf71bdeb20",
+    "url": "/static/js/runtime-main.4c039259.js"
+  }
+]);
